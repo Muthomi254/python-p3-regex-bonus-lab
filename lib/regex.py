10df163830f124f77f3c5bca7de3f6be7a0abe95
muthomi254 <dmunene202020@gmail.com>
2023-12-08 23:44:15 +0300

    partialy completed

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,33 @@
+# import re
+
+# my_pattern = r""
+# my_regex = re.compile(my_pattern)
 import re
 
-my_pattern = r""
+# Your refined regular expression pattern
+my_pattern = r".*"  # Matches lines ending with "today."
+
+# Compile the regex pattern
 my_regex = re.compile(my_pattern)
 
+# Test using the fullmatch method
+result = my_regex.fullmatch("It's such a lovely day today.")
+print(result.group() if result else None)  # Should print the entire string or None
+
+# Strings to be used for findall test
+# FINDALL_STRING = """
+#     It's such a lovely day today.
+#     Some weather we're having today, huh?
+#     Maybe today's just not my day.
+#     Where'd all the time go?
+# """
+
+# # Test using the findall method
+# matches = my_regex.findall(FINDALL_STRING)
+# expected_matches = [
+#     "It's such a lovely day today.",
+#     "Some weather we're having today, huh?",
+#     "Maybe today's just not my day.",
+# ]
+
+# assert matches == expected_matches
